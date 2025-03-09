68acba9c7c446e7e95e649b9fbf0b44077751070
Juan Guerra <juan.l.guerra@outlook.com>
2025-03-09 10:25:43 -0500

    cleanup script removes extra features and  normalizes prices to USD/lb

--- data/com_data_clean.py
+++ data/com_data_clean.py
@@ -4,12 +4,8 @@ file_path = "commodity_prices.csv"
 df = pd.read_csv(file_path)
 
 commodities = [
-    "Tea, Kolkata ",
-    "Tea, Colombo ",
     "Tea, Kenyan ",
-    "Tea, Mombasa ",
     "Coffee, Other Mild Arabica ",
-    "Coffee, Robustas ",
     "Dairy Products, Milk",
     "Sugar, No. 11, World "
 ]
@@ -20,9 +16,17 @@ monthly_columns = [col for col in df.columns if "M" in col and (col.startswith("
 columns_to_keep = ["Commodity Name"] + monthly_columns
 df_monthly = df_filtered[columns_to_keep]
 
+for index, row in df_monthly.iterrows():
+    commodity = row["Commodity Name"].strip()
+    if commodity in ["Coffee, Other Mild Arabica", "Coffee, Robustas", "Sugar, No. 11, World", "Dairy Products, Milk"]:
+        df_monthly.loc[index, monthly_columns] = row[monthly_columns] / 100
+    elif "Tea, Kenyan" in commodity:
+        df_monthly.loc[index, monthly_columns] = (row[monthly_columns] / 2.20462) / 100
+        # normalizes the price of all commodities to USD per pound
+
 df_transposed = df_monthly.set_index("Commodity Name").T
 df_transposed.dropna(axis=1, how='all', inplace=True)
-df_transposed.index.name = "Year"
+df_transposed.index.name = "Year/Month"
 
 output_file = "global_commodity_prices.csv"
 df_transposed.to_csv(output_file)
