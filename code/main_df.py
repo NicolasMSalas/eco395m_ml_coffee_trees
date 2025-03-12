03d2690b66fc230f2b19a555bcb5af2fdf88c48b
Juan Guerra <juan.l.guerra@outlook.com>
2025-03-12 15:13:15 -0500

    main_df updated with all features

--- code/main_df.py
+++ code/main_df.py
@@ -6,6 +6,20 @@ from sklearn.impute import IterativeImputer
 notebook_dir = os.path.dirname(os.path.abspath("__file__"))
 csv_path = os.path.join(notebook_dir, '..', 'clean_data')
 
+def load_weatcher_data(filename, suffix):
+    """Load weather data from csv file and add suffix to columns"""
+    full_path = os.path.join(csv_path, filename)
+    df = pd.read_csv(full_path)
+    df = df.add_suffix(suffix)
+    df.rename(columns={f'Date{suffix}': 'Date'}, inplace=True)
+    return df
+
+df_br = load_weatcher_data('Brazil_weather_cleaned.csv', '_br')
+df_col = load_weatcher_data('Colombia_weather_cleaned.csv', '_col')
+df_vn = load_weatcher_data('Vietnam_weather_cleaned.csv', '_vn')
+df_hon = load_weatcher_data('Honduras_weather_cleaned.csv', '_hon')
+df_in = load_weatcher_data('Indonesia_weather_cleaned.csv', '_in')
+
 def load_exchange_data(filename):
     """Load currency exchange data from csv file"""
     full_path = os.path.join(csv_path, filename)
@@ -28,7 +42,7 @@ commodity_path = os.path.join(csv_path, 'commodity_prices_clean.csv')
 df_commodity = pd.read_csv(commodity_path)
 
 merged_df = df_brl.copy()
-for df in [df_vnd, df_hnl, df_idr, df_commodity, df_cop, df_sentiment]:
+for df in [df_vnd, df_hnl, df_idr, df_commodity, df_cop, df_sentiment, df_futures, df_col, df_vn, df_hon, df_in]:
     merged_df = pd.merge(merged_df, df, on="Date", how="inner")
     
 imputer = IterativeImputer(random_state=42)
@@ -42,4 +56,4 @@ output_file = os.path.join(output_dir, "main_df_clean.csv")
 
 os.makedirs(output_dir, exist_ok=True)
 
-merged_df.to_csv(output_file, index=True)
+merged_df.to_csv(output_file, index=False)
